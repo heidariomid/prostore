0c2ee6de65d22ece53ea398887f9c2dedf7b8107
Omid Heidari <heidariomid.dev@gmail.com>
2025-04-29 14:40:22 +0330

    Add Postgress

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
 import ProductList from "@/components/ui/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
       <div className="space-y-8">
         <h2 className="h2-bold">Latest Products</h2>
-        <ProductList title="Newest Arrivals" data={sampleData.products} limit={0} />
+        <ProductList title="Newest Arrivals" data={latestProducts} />
       </div>
     </>
   );
---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,14 @@
+"use server";
+import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+import { convertToPlainObject } from "../utils";
+
+// Get the latest products
+export async function getLatestProducts() {
+  const prisma = new PrismaClient();
+  const data = await prisma.product.findMany({
+    orderBy: { createdAt: "desc" }
+  });
+  return convertToPlainObject(data);
+}
