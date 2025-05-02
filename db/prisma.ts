eda2423a25aa22e356997e3cef7b842f3cc7dbd2
Omid Heidari <heidariomid.dev@gmail.com>
2025-05-02 17:51:16 +0330

    add authentication

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
@@ -11,10 +12,10 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool);
+// const adapter = new PrismaNeon(pool);
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-export const prisma = new PrismaClient({ adapter }).$extends({
+export const prisma = new PrismaClient().$extends({
   result: {
     product: {
       price: {
